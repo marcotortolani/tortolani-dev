97a80c19b667f0efd2939e9ebc2d836fdc0c6fd0
Marco Tortolani <mjtorto@gmail.com>
2023-08-11 13:32:12 -0300

    ✚ adding tailwind

--- src/app.jsx
+++ src/app.jsx
@@ -4,8 +4,8 @@ import './app.css'
 export function App() {
 
   return (
-    <>
-      <h1>Marco Tortolani</h1>
-    </>
+    <div className=' w-screen h-screen overflow-x-hidden flex flex-col items-center bg-slate-800'>
+      <h1 className=' text-3xl text-cyan-600 '>Marco Tortolani</h1>
+    </div>
   )
 }
